b2bfe4f88331b34ed154ec6f8765600bfc342133
Hikmet <hikmetmut@gmail.com>
2022-04-12 11:40:00 +0300

    12/04/2022-2

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,9 +1,10 @@
 ﻿
 using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 
 
-ProductManager productManager = new ProductManager(new InMemoryProductDal());
+ProductManager productManager = new ProductManager(new EfProductDal());
 
 foreach (var item in productManager.GetAll())
 {
---
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,64 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal : IProductDal
+    {
+        public void Add(Product entity)
+        {
+            //IDisposible pattern implementation of c#
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var addedEntity=context.Entry(entity); // yakala
+                addedEntity.State = EntityState.Added; // ekle
+                context.SaveChanges(); // kaydet
+            }
+        }
+
+        public void Delete(Product entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var deletedEntity = context.Entry(entity); // yakala
+                deletedEntity.State = EntityState.Deleted; // sil
+                context.SaveChanges(); // kaydet
+            }
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                return context.Set<Product>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            using (NorthwindContext context=new NorthwindContext())
+            {
+                return filter == null 
+                    ? context.Set<Product>().ToList() 
+                    : context.Set<Product>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(Product entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var updateEntity = context.Entry(entity); // yakala
+                updateEntity.State = EntityState.Modified; // güncelle
+                context.SaveChanges(); // kaydet
+            }
+        }
+    }
+}
