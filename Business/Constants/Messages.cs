2a581e9a644fcb216840a075617e36ad0f14ecc1
Hikmet <hikmetmut@gmail.com>
2022-04-14 15:50:20 +0300

    14/04/2022-son

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün Eklendi";
+        public static string ProductNameInValid = "Ürün Geçersiz";
+        public static string MaintenanceTime = "Sistem Bakım Saati";
+        public static string ProductListed = "Ürünler Listelendi";
+        
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -12,10 +12,20 @@ using DataAccess.Concrete.InMemory;
 //}
 
 ProductManager productManager = new ProductManager(new EfProductDal());
-foreach (var item in productManager.GetProductDetails())
+
+var result = productManager.GetProductDetails();
+if (result.Success)
 {
-    Console.WriteLine(item.ProductName +" --------" + item.CategoryName);
+    foreach (var item in productManager.GetProductDetails().Data)
+    {
+        Console.WriteLine(item.ProductName + " --------" + item.CategoryName);
+    }
 }
+else
+{
+    Console.WriteLine(result.Message);
+}
+
 
 
 
